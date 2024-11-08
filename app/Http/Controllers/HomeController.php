d847c265cdf697d9d1dc4bda365b539141ff5072
Muchirikinyua <Muchirikinyua2000@gmail.com>
2024-11-08 06:59:05 +0300

    Title LearnsoftCRM

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Http\Request;
 
@@ -23,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user_name = Auth::user()->name;
+        return view('home', ['user_name' => $user_name]); 
+        
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.app')
-
+<title>LearnsoftCRM</title>
 @section('content')
     <div class="container-fluid">
         <h1 class="text-black-50">You are logged in!</h1>
+        <h2>Welcome, {{$user_name}}!</h2>
     </div>
 @endsection
