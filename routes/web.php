ca2c68db384ef2f91344451b106358ed0f5a70b8
victoroki <okiomeriv@gmail.com>
2024-11-15 09:28:17 +0300

    order validation

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LeadController;
 use App\Http\Controllers\OrderController;
+use App\Models\Product;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,5 +35,13 @@ Route::resource('transactions', App\Http\Controllers\TransactionController::clas
 Route::get('/clients/{id}', [ClientController::class, 'show'])->name('clients.show');
 Route::get('leads/{id}/convert', [LeadController::class, 'convertToClient'])->name('leads.convertToClient');
 Route::get('leads/{id}/convert-to-client', [LeadController::class, 'convertToClient'])->name('leads.convertToClient');
+
+Route::get('/get-product-price/{id}', function ($id) {
+    $product = Product::find($id);
+    if ($product) {
+        return response()->json(['price' => $product->price]);
+    }
+    return response()->json(['error' => 'Product not found'], 404);
+});
 
 Route::get('/get-order-data', [OrderController::class, 'getOrderData']);
