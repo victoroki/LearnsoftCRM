11cf802714ba85c155526f9b4169a228c2472534
Anzelah <jepkoechanzellah@gmail.com>
2024-11-19 18:14:25 +0300

    change to table.php

--- resources/views/clients/table.blade.php
+++ resources/views/clients/table.blade.php
@@ -15,7 +15,7 @@
             <tr>
                 <td>{{ $client->full_name }}</td>
                 <td>{{ $client->company_name }}</td>
-                <td>{{ $client->email_address }}</td>
+                <td id="email">{{ $client->email_address }}</td>
                 <td>{{ $client->phone_number }}</td>
                 <td>{{ $client->lead->name ?? 'No Lead' }}</td>
                 <td>{{ $client->location }}</td>
