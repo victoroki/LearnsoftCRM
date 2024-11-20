fe904be6bb9c7b35842e780b687bba7f0da98d86
OG-Oluoch <makopiyo45@gmail.com>
2024-11-20 06:30:19 +0300

    changed the home views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,10 +18,10 @@
                     <div class="card bg-gradient-primary">
                         <p class="h3 mb-1 text-black">{{ $totalClients }}</p>
                         <p class="text-black">Clients</p>
-                        <hr>
+                        <i class="nav-icon fas fa-address-book"></i>
                         <hr>
                         <form action="{{route('clients.index')}}" method="GET">
-                        <button type="submit" >View clients</button>
+                        <button type="submit" class="btn btn-dark">View clients</button>
                         </form>
                         </div>
                     </div>
@@ -33,10 +33,10 @@
                     <div class="card bg-gradient-success">
                         <p class="h3 mb-1 text-black">{{ $totalProducts }}</p>
                         <p class="text-black">Products</p>
-                        <hr>
+                        <i class="nav-icon fas fa-shopping-cart"></i>
                         <hr>
                         <form action="{{route('products.index')}}" method="GET">
-                        <button type="submit" >View products</button>
+                        <button type="submit" class="btn btn-dark" >View products</button>
                         </form>
                         </div>
                     </div>
@@ -48,10 +48,10 @@
                     <div class="card bg-gradient-warning">
                         <p class="h3 mb-1 text-black">{{ $totalOrders }}</p>
                         <p class="text-black">Orders</p>
-                        <hr>
+                        <i class="nav-icon fas fa-shopping-basket"></i>
                         <hr>
                         <form action="{{route('orders.index')}}" method="GET">
-                        <button type="submit" >View orders</button>
+                        <button type="submit" class="btn btn-dark" >View orders</button>
                         </form>
                         </div>
                     </div>
@@ -60,13 +60,13 @@
             <div class="col-md-4">
                 <div class="card shadow">
                     <div class="card-body">
-                    <div class="card bg-gradient-primary">
+                    <div class="card bg-gradient-secondary">
                         <p class="h3 mb-1 text-black">{{ $totalDepartments }}</p>
                         <p class="text-black">Departments</p>
-                        <hr>
+                        <i class="nav-icon fas fa-users"></i>
                         <hr>
                         <form action="{{route('departments.index')}}" method="GET">
-                        <button type="submit" >View departments</button>
+                        <button type="submit" class="btn btn-dark">View departments</button>
                         </form>
                         </div>
                     </div>
@@ -75,13 +75,13 @@
             <div class="col-md-4">
                 <div class="card shadow">
                     <div class="card-body">
-                      <div class="card bg-gradient-primary">
+                      <div class="card bg-info">
                         <p class="h3 mb-1 text-black">{{ $totalEmployees }}</p>
                         <p class="text-black">Employees</p>
-                        <hr>
+                        <i class="nav-icon fas fa-user"></i>
                         <hr>
                         <form action="{{route('employees.index')}}" method="GET">
-                        <button type="submit" >View employees</button>
+                        <button type="submit" class="btn btn-dark">View employees</button>
                         </form>
                           </div>
                         </div>
@@ -90,12 +90,13 @@
             <div class="col-md-4">
                 <div class="card shadow">
                     <div class="card-body">
-                    <div class="card bg-gradient-primary">
+                    <div class="card bg-light">
                         <p class="h3 mb-1 text-black">{{ $totalInteractions }}</p>
                         <p class="text-black">Interactions</p>
+                        <i class="nav-icon fas fa-tasks"></i>
                         <hr>
                         <form action="{{route('interactions.index')}}" method="GET">
-                        <button type="submit" >View interactions</button>
+                        <button type="submit" class="btn btn-dark">View interactions</button>
                         </form>
                         </div>
                     </div>
