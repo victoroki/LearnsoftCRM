bd1f913f1532a225b46aecd8049f6d768a73deb0
EmmaculateKatheu <emmakatheu123@gmail.com>
2024-11-22 12:33:07 +0300

    changed the package and app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,7 @@ import './bootstrap';
 import 'admin-lte';
 
 import $ from 'jquery';
-import 'jquery-ui/ui/widgets/datepicker';  // Import datepicker widget
+import 'jquery-ui/ui/widgets/datepicker';
 
 $(document).ready(function() {
     $('#lead_date').datepicker({
