291bbc6d07c6185b0765a667bf028be40976bdcd
OG-Oluoch <makopiyo45@gmail.com>
2024-11-25 01:51:45 +0300

    views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,25 +3,23 @@
 @section('content')
 
 <head>
-    
-<style>
-h1,p{
-    text-align: center;
-}
-    </style>
+
+
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <link href="{{asset('css\myUI.css')}}" type="text/css" 
+          rel="stylesheet" />
    
 </head>
+
 <div class="container-fluid">
     <h1 class="text-black">Welcome {{ Auth::user()->name }}</h1>
     <p>{{ \Carbon\Carbon::now()->format('F j, Y') }}</p>
     
     <div class="container-fluid-1">
         <div class="row-1">
-            <div class="col">
-              
-                    <div class="card-body" style="width: 425px; height:175px">
+           
+              <div class="card-body">
                     <div class="card bg-gradient-primary">
                         <p class="h3 mb-1 text-black">{{ $totalClients }}</p>
                         <p class="text-black">Clients</p>
@@ -30,13 +28,13 @@ h1,p{
                         <form action="{{route('clients.index')}}" method="GET">
                         <button type="submit" class="btn btn-dark">View clients</button>
                         </form>
-                        </div>
                     </div>
-                
-            </div>
-            <div class="col">
+                </div>
+           </div>
+
+            <div class="row-1">
             
-                    <div class="card-body"style="width: 425px; height:175px">
+                    <div class="card-body">
                     <div class="card bg-gradient-success">
                         <p class="h3 mb-1 text-black">{{ $totalProducts }}</p>
                         <p class="text-black">Products</p>
@@ -49,9 +47,9 @@ h1,p{
                     </div>
                 
             </div>
-            <div class="col">
+            <div class="row-1">
             
-                    <div class="card-body" style="width: 425px; height:175px">
+                    <div class="card-body">
                     <div class="card bg-gradient-warning">
                         <p class="h3 mb-1 text-black">{{ $totalOrders }}</p>
                         <p class="text-black">Orders</p>
@@ -63,14 +61,13 @@ h1,p{
                         </div>
                     </div>
                </div>
-        </div>
     </div>
 
-    <div class="container-fluid-1">
-    <div class="row-1">
-            <div class="col">
+    <div class="container-fluid-2">
+    
+            <div class="row-2">
             
-                    <div class="card-body" style="width: 425px; height:175px">
+                    <div class="card-body">
                     <div class="card bg-gradient-secondary">
                         <p class="h3 mb-1 text-black">{{ $totalDepartments }}</p>
                         <p class="text-black">Departments</p>
@@ -83,9 +80,9 @@ h1,p{
                     </div>
                
             </div>
-            <div class="col">
+            <div class="row-2">
             
-                    <div class="card-body" style="width: 425px; height:175px">
+                    <div class="card-body">
                       <div class="card bg-info">
                         <p class="h3 mb-1 text-black">{{ $totalEmployees }}</p>
                         <p class="text-black">Employees</p>
@@ -98,9 +95,9 @@ h1,p{
                         </div>
                    
             </div>
-            <div class="col">
+            <div class="row-2">
             
-                    <div class="card-body" style="width: 425px; height:175px">
+                    <div class="card-body">
                     <div class="card bg-light">
                         <p class="h3 mb-1 text-black">{{ $totalInteractions }}</p>
                         <p class="text-black">Interactions</p>
@@ -113,39 +110,37 @@ h1,p{
                    
                 </div>
             </div>
-    </div>
-    </div>
+     </div>
 
-    <div class="container-fluid-2">
+    <div class="container-fluid-3">
 
-        <div class="row-1">
-        <div class= "col-1">
-            <div class="card-body"style="width:250px; height:250px; margin:auto;">
+        <div class="row-3">
+        
+            <div class="card-body">
              @include('partials.summaryChart')
-             </div>
-             </div>
-     
-  <div class= "col-1"> 
+              </div>
+        </div>
+
+  <div class= "row-3"> 
     <div class="card-body">
     @include('partials.infoChart') 
     </div>
   </div>
+
+  
+  <div class="row-3">
+      <div class="card-body"> @include('partials.chart') </div>
   </div>
-   <div class="container-fluid-2">
-    <div class="row-1">
-    <div class="col-1">
-  <div class="card-body"> @include('partials.chart') </div>
-  </div>
-   <div class="col-1">
+
+  <div class="row-3">
   <div class="card-body"> @include('partials.myChart') </div>
   </div>
-   </div>
-   </div>
+
 
   </div>
+  
 
-  </div>
     
-
 </div>
+
 @endsection
